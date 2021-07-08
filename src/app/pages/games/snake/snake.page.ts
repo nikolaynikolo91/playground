64039eda4da3feecb8c3c9b8c9b9dc33cda7f6e0
nikolaynikolo91 <57035504+nikolaynikolo91@users.noreply.github.com>
2021-07-08 22:52:17 +0300

    add random food

--- src/app/pages/games/snake/snake.page.ts
+++ src/app/pages/games/snake/snake.page.ts
@@ -19,6 +19,7 @@ import { tap, withLatestFrom } from 'rxjs/operators';
   styleUrls: ['./snake.page.scss'],
 })
 export class SnakePage implements AfterViewInit {
+  @ViewChild('gameBoard') gameBoard: ElementRef;
   @ViewChild('square') square: ElementRef;
   @ViewChildren('tail') tail: QueryList<any>;
   anim: Animation;
@@ -27,6 +28,8 @@ export class SnakePage implements AfterViewInit {
   private y = 0;
   private direction = new BehaviorSubject('RIGHT');
   private movingDirection = '';
+  private foodX: number;
+  private foodY: number;
 
   constructor(
     private animationCtrl: AnimationController,
@@ -34,7 +37,24 @@ export class SnakePage implements AfterViewInit {
   ) {}
 
   ngAfterViewInit() {
+    this.food();
     this.move();
+    //  console.log(this.getRandomInt(0, 500));
+    console.log(this.gameBoard.nativeElement.children);
+    console.log('da');
+  }
+
+  food() {
+    this.foodX = this.getRandomInt(0, 499);
+    this.foodY = this.getRandomInt(0, 499);
+    const foods = this.renderer.createElement('div');
+    foods.classList.add('square');
+    this.renderer.setStyle(
+      foods,
+      'transform',
+      `translate(${this.foodX}px,${this.foodY}px)`
+    );
+    this.renderer.appendChild(this.gameBoard.nativeElement, foods);
   }
 
   move() {
@@ -68,6 +88,29 @@ export class SnakePage implements AfterViewInit {
               break;
           }
 
+          if (this.x > 499) {
+            this.x = 0;
+          }
+
+          if (this.y > 499) {
+            this.y = 0;
+          }
+
+          if (this.x < 0) {
+            this.x = 499;
+          }
+
+          if (this.y < 0) {
+            this.y = 499;
+          }
+
+          if (this.foodX === this.x && this.y === this.foodY){
+           const newTail = this.renderer.createElement('div');
+           this.renderer.addClass(newTail, '')
+          }
+
+          
+
           this.renderer.setStyle(
             this.square.nativeElement,
             'transform',
@@ -76,6 +119,12 @@ export class SnakePage implements AfterViewInit {
         })
       )
       .subscribe();
+  }
+
+  getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
   }
 
   toggleAnimationDown() {
