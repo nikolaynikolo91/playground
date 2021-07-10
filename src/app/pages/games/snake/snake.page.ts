0a70a9f82a4b9924e2b3a587c9007c145acc1ea9
nikolaynikolo91 <57035504+nikolaynikolo91@users.noreply.github.com>
2021-07-10 22:13:35 +0300

    add snake grow

--- src/app/pages/games/snake/snake.page.ts
+++ src/app/pages/games/snake/snake.page.ts
@@ -20,35 +20,35 @@ import { tap, withLatestFrom } from 'rxjs/operators';
 })
 export class SnakePage implements AfterViewInit {
   @ViewChild('gameBoard') gameBoard: ElementRef;
-  @ViewChild('square') square: ElementRef;
-  @ViewChildren('tail') tail: QueryList<any>;
-  anim: Animation;
-  isPlaying = false;
+  @ViewChild('snake') snake: ElementRef;
+  @ViewChild('head') head: ElementRef;
+  // @ViewChildren('tail') tail: QueryList<any>;
+
   private x = 0;
   private y = 0;
   private direction = new BehaviorSubject('RIGHT');
   private movingDirection = '';
   private foodX: number;
   private foodY: number;
+  private tailX: number;
+  private tailY: number;
+  private tail: ElementRef[] = [];
 
-  constructor(
-    private animationCtrl: AnimationController,
-    private renderer: Renderer2
-  ) {}
+  constructor(private renderer: Renderer2) {}
 
   ngAfterViewInit() {
     this.food();
     this.move();
     //  console.log(this.getRandomInt(0, 500));
-    console.log(this.gameBoard.nativeElement.children);
-    console.log('da');
+    // const fullSnake = this.snake.nativeElement.children;
+    console.log(this.snake.nativeElement.children);
   }
 
   food() {
-    this.foodX = this.getRandomInt(0, 499);
-    this.foodY = this.getRandomInt(0, 499);
+    this.foodX = this.getRandom10();
+    this.foodY = this.getRandom10();
     const foods = this.renderer.createElement('div');
-    foods.classList.add('square');
+    foods.classList.add('food');
     this.renderer.setStyle(
       foods,
       'transform',
@@ -58,16 +58,14 @@ export class SnakePage implements AfterViewInit {
   }
 
   move() {
-    timer(0, 100)
+    const snakeHead = this.snake.nativeElement.firstChild;
+
+    timer(0, 200)
       .pipe(
         withLatestFrom(this.direction),
         tap(([, direction]) => {
-          this.movingDirection = direction;
-          this.renderer.setStyle(
-            this.tail.last.nativeElement,
-            'transform',
-            `translate(${this.x}px,${this.y}px)`
-          );
+          this.tailX = this.x;
+          this.tailY = this.y;
 
           switch (direction) {
             case 'UP':
@@ -88,6 +86,7 @@ export class SnakePage implements AfterViewInit {
               break;
           }
 
+          // check gameBoarder
           if (this.x > 499) {
             this.x = 0;
           }
@@ -104,21 +103,53 @@ export class SnakePage implements AfterViewInit {
             this.y = 499;
           }
 
-          if (this.foodX === this.x && this.y === this.foodY){
-           const newTail = this.renderer.createElement('div');
-           this.renderer.addClass(newTail, '')
-          }
-
-          
-
           this.renderer.setStyle(
-            this.square.nativeElement,
+            snakeHead,
             'transform',
             `translate(${this.x}px,${this.y}px)`
           );
+
+          const foodBoard = this.gameBoard.nativeElement.lastChild;
+
+          // check take food and snake grow
+          if (this.foodX === this.x && this.foodY === this.y) {
+            const newTail = this.renderer.createElement('div');
+            this.renderer.addClass(newTail, 'tail');
+            this.renderer.setStyle(
+              newTail,
+              'transform',
+              `translate(${this.tailX}px,${this.tailY}px)`
+            );
+            this.renderer.appendChild(this.snake.nativeElement, newTail);
+            this.tail.push(newTail);
+
+            this.foodX = this.getRandom10();
+            this.foodY = this.getRandom10();
+
+            this.renderer.setStyle(
+              foodBoard,
+              'transform',
+              `translate(${this.foodX}px,${this.foodY}px)`
+            );
+          }
+
+          if (this.tail.length > 0) {
+            this.tail.unshift(this.tail.pop());
+
+            this.renderer.setStyle(
+              this.tail[this.tail.length - 1],
+
+              'transform',
+              `translate(${this.tailX}px,${this.tailY}px)`
+            );
+          }
         })
       )
       .subscribe();
+  }
+
+  getRandom10() {
+    return this.getRandomInt(1, 50) * 10;
   }
 
   getRandomInt(min: number, max: number) {
